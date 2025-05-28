d149e6694120bf638aa66f901c7fb46e6cb6ae11
Joelle <rladusdn56@gmail.com>
2025-05-28 16:56:43 +0900

    Feat: 결과화면 구현

--- nunSong/Assets/1_init/Scripts/GameManager.cs
+++ nunSong/Assets/1_init/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -16,6 +17,29 @@ public class GameManager : MonoBehaviour
     }
 
     E_STATE gamestate; //게임 상태 변수
+    public static GameManager Instance { get; private set; } //싱글톤 인스턴스
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); //씬 전환 시에도 파괴되지 않도록 설정
+        }
+        else
+        {
+            Destroy(gameObject); //이미 인스턴스가 존재하면 중복 생성 방지
+        }
+    }
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
 
     [Header("UI")]
     public GameObject ui_gameIntro; //게임 로고 및 온보딩 화면
@@ -34,7 +58,89 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
-        gamestate = E_STATE.INTRO;
+        string sceneState = PlayerPrefs.GetString("InitSceneState", "INTRO");
+
+        switch (sceneState)
+        {
+            case "SELECTSONG":
+                gamestate = E_STATE.SELECTSONG;
+                break;
+            case "SONGDETAIL":
+                gamestate = E_STATE.SONGDETAIL;
+                break;
+            case "INTRO":
+            default:
+                gamestate = E_STATE.INTRO;
+                break;
+        }
+
+        PlayerPrefs.DeleteKey("InitSceneState");
+
+        // 별도 처리: SongDetailUI 활성화 여부
+        if (PlayerPrefs.GetInt("ActivateSongDetailUI", 0) == 1)
+        {
+            PlayerPrefs.SetInt("ActivateSongDetailUI", 0);
+            gamestate = E_STATE.SONGDETAIL;
+        }
+        Debug.Log($"[GameManager] InitSceneState = {sceneState}, gamestate = {gamestate}");
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "InitScene")
+        {
+            Debug.Log("[GameManager] InitScene 로드 감지");
+
+            // UI 다시 연결
+            StartCoroutine(DelayedReconnect());
+
+            // 상태 복원
+            string sceneState = PlayerPrefs.GetString("InitSceneState", "INTRO");
+            switch (sceneState)
+            {
+                case "SELECTSONG":
+                    gamestate = E_STATE.SELECTSONG;
+                    break;
+                case "SONGDETAIL":
+                    gamestate = E_STATE.SONGDETAIL;
+                    break;
+                case "INTRO":
+                default:
+                    gamestate = E_STATE.INTRO;
+                    break;
+            }
+
+            PlayerPrefs.DeleteKey("InitSceneState");
+            Debug.Log($"[GameManager] InitSceneState = {sceneState}, gamestate = {gamestate}");
+
+            if (PlayerPrefs.GetInt("ActivateSongDetailUI", 0) == 1)
+            {
+                PlayerPrefs.SetInt("ActivateSongDetailUI", 0);
+                gamestate = E_STATE.SONGDETAIL;
+            }
+        }
+    }
+
+    private IEnumerator DelayedReconnect()
+    {
+        yield return new WaitForSeconds(0.1f); // 씬 생성 딜레이 고려
+        ReconnectUIReferences();
+        Debug.Log("<color=green>GameManager UI 재연결 완료!</color>");
+    }
+
+    private void ReconnectUIReferences()
+    {
+        ui_gameIntro = GameObject.Find("IntroUI");
+        ui_mainCover = GameObject.Find("mainUI");
+        ui_setting = GameObject.Find("SettingUI");
+        ui_songSelect = GameObject.Find("SelectUI");
+        ui_songDetail = GameObject.Find("SongDetailUI");
+
+        setting_ui_noteSpeed = GameObject.Find("noteSpeedTab");
+        setting_ui_correction = GameObject.Find("latencyTab");
+        setting_ui_sound = GameObject.Find("soundTab");
+        setting_ui_key = GameObject.Find("keyTab");
+        setting_ui_graphics = GameObject.Find("graphicsTab");
     }
 
     void Update()
@@ -53,12 +159,12 @@ public class GameManager : MonoBehaviour
         {
             case E_STATE.NONE:
                 break;
-            
+
             case E_STATE.INTRO:
                 gamestate = E_STATE.NONE;
                 StartCoroutine(GameIntro());
                 break;
-            
+
             case E_STATE.HOMEMAIN:
                 gamestate = E_STATE.NONE;
                 GameHomeMain();
@@ -68,21 +174,22 @@ public class GameManager : MonoBehaviour
                 gamestate = E_STATE.NONE;
                 Settings();
                 break;
-            
+
             case E_STATE.SELECTSONG:
                 gamestate = E_STATE.NONE;
                 SongSelect();
                 break;
-            
+
             case E_STATE.SONGDETAIL:
                 gamestate = E_STATE.NONE;
                 SongDetail();
                 break;
-            
+
             case E_STATE.END:
                 GameEnd();
                 break;
         }
+        PlayerPrefs.DeleteKey("InitSceneState");
     }
 
     IEnumerator GameIntro()
@@ -224,7 +331,7 @@ public class GameManager : MonoBehaviour
         ui_songDetail.SetActive(true); //곡 상세 화면을 띄움
     }
 
-        public void OnClickBacktoSongSelect()
+    public void OnClickBacktoSongSelect()
     {
         Debug.Log("뒤로가기 버튼 누름");
         gamestate = E_STATE.SELECTSONG;
@@ -235,4 +342,62 @@ public class GameManager : MonoBehaviour
         Debug.Log("게임종료");
         // Application.Quit(); //게임 종료
     }
+
+    public void onClickGameResult()
+    {
+        Debug.Log("게임 결과 화면으로 이동");
+        SceneManager.LoadScene("ResultScene");
+    }
+
+    // 메인화면으로 이동 (InitScene)
+    public void OnClickGoToMainScene()
+    {
+        Debug.Log("메인화면으로 이동");
+        PlayerPrefs.SetString("InitSceneState", "INTRO");
+        SceneManager.LoadScene("InitScene");
+    }
+
+    // 재시작 (PlayScene)
+    public void OnClickRestartGame()
+    {
+        Debug.Log("게임 재시작");
+        SceneManager.LoadScene("PlayScene");
+    }
+
+    // 곡선택 > InitScene 으로 이동 후 songDetailUI 활성화 요청
+    public void OnClickSelectSongDetailFromMain()
+    {
+        Debug.Log("InitScene으로 이동 후 SongDetailUI 활성화 요청");
+        PlayerPrefs.SetString("InitSceneState", "SELECTSONG");
+        PlayerPrefs.SetInt("ActivateSongDetailUI", 1); // 표시 플래그 저장
+        SceneManager.LoadScene("InitScene");
+    }
+    
+    public void BindUI(
+    GameObject intro,
+    GameObject main,
+    GameObject setting,
+    GameObject select,
+    GameObject detail,
+    GameObject note,
+    GameObject latency,
+    GameObject sound,
+    GameObject key,
+    GameObject graphics
+)
+{
+    ui_gameIntro = intro;
+    ui_mainCover = main;
+    ui_setting = setting;
+    ui_songSelect = select;
+    ui_songDetail = detail;
+
+    setting_ui_noteSpeed = note;
+    setting_ui_correction = latency;
+    setting_ui_sound = sound;
+    setting_ui_key = key;
+    setting_ui_graphics = graphics;
+
+    Debug.Log("<color=green>GameManager: UI 재연결 완료!</color>");
+}
 }
