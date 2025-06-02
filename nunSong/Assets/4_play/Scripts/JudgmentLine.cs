17401a7c66669e518869c17c226439ab2109cc5d
JEcode-2002 <codingje@gmail.com>
2025-06-03 06:06:43 +0900

    노트 수작업 완료

--- nunSong/Assets/4_play/Scripts/JudgmentLine.cs
+++ nunSong/Assets/4_play/Scripts/JudgmentLine.cs
@@ -6,8 +6,8 @@ public class JudgmentLine : MonoBehaviour
 
     // 접근 가능한 범위 값들 (인스펙터에 노출 방지)
     [HideInInspector] public readonly float perfectRange = 50f;  // ±50ms=0.05f로 추후 변경, 아래도 동일
-    [HideInInspector] public readonly float greatRange = 100f;    // ±80ms
-    [HideInInspector] public readonly float goodRange = 150f;     // ±120ms
+    [HideInInspector] public readonly float greatRange = 70f;    // ±80ms
+    [HideInInspector] public readonly float goodRange = 100f;     // ±120ms
 
     private void Awake()
     {
